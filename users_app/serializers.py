e402dd662a0e4b50d2d41a43c4249d69b1a28844
Parsa <66327578+parsarmx@users.noreply.github.com>
2022-01-15 22:48:03 +0330

    ProfileSeializers added and view changed!

---
+++ users_app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Profile
+
+
+class ProfileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'email',
+            'birth_day',
+        )
--- users_app/views.py
+++ users_app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+
+from .serializers import ProfileSerializers
+from .models import Profile
+
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # Create your views here.
-class ProfileAPIView(GenericAPIView):
+class ProfileAPIView(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get(self, request, u_name):
-        return Response(data={'message': f"Hello {request.user.username}"})
+        user = Profile.objects.get(username=request.user.username)
+        serializers = ProfileSerializers(user)
+        return Response(serializers.data)
