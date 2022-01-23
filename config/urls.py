04f662930756bb033e80e60e98fca47868597a52
Parsa <66327578+parsarmx@users.noreply.github.com>
2022-01-23 17:00:45 +0330

    update urls for using uwsgi

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 from users_app.views import ProfileAPIView
 from rest_framework import permissions
@@ -26,4 +28,4 @@ urlpatterns = [
     path('', include('users_app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
